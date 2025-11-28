9a56ea280084bd18be122074a36b1fe317642c80
rsim08-svg <r.sim08@mail.ru>
2025-11-28 19:32:17 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include<stdbool.h>
+#include "main.h"
+
+int calc(int i1, int i2, char op)
+{
+    if (op == ((int)('+'))) return i1 + i2;
+    if (op == ((int)('-'))) return i1 - i2;
+    if (op == ((int)('*'))) return i1 * i2;
+    if (op == ((int)('%'))) return i1 % i2;
+    return 0;
+}
+
+void result(char* arr, int len, int k)
+{
+    for(int j = 0; j < len; j++) arr[j] -= k;
+}
---
+++ main.h
@@ -0,0 +1,4 @@
+#pragma once
+int calc(int i1, int i2, char op);
+void result(char* arr, int len, int k);
+
